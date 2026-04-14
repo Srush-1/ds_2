0adfd223023224f848b09c4a09490799b0b61b81
Srush-1 <srushtiingle50@gmail.com>
2026-04-14 12:21:36 +0530

    added

---
+++ p1.py
@@ -0,0 +1,2 @@
+for i in range(1,11):
+    print("square of num",i*i)
---
+++ p2.py
@@ -0,0 +1,6 @@
+s=input("enter a string")
+rev=s[::-1]
+if(s==rev):
+    print("given string is palindrome")
+else:
+    print("string is not palindrome")
